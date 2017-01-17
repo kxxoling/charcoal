b42281399455639245d9ff11567c981e8baeac6f
kxxoling <kxxoling@gmail.com>
2017-01-17 16:37:58 +0800

    Fix page content not displayed on homepage issue.

--- home/models.py
+++ home/models.py
@@ -2,11 +2,14 @@ from wagtail.wagtailcore.models import Page
 from posts.models import ArticlePage, GalleryPage, VideoPage, SharedLinkPage
 
 
+PAGE_TYPES = (ArticlePage, GalleryPage, VideoPage, SharedLinkPage,)
+
+
 class HomePage(Page):
+    subpage_types = list(PAGE_TYPES)
 
     def get_context(self, request):
         context = super(HomePage, self).get_context(request)
-        context['posts'] = Page.objects.type((ArticlePage, GalleryPage, VideoPage, SharedLinkPage)).live()
-        context['recent_posts'] = Page.objects.type((
-            ArticlePage, GalleryPage, VideoPage, SharedLinkPage)).live()
+        context['posts'] = Page.objects.type(PAGE_TYPES).live()
+        context['recent_posts'] = Page.objects.type(PAGE_TYPES).live()
         return context
