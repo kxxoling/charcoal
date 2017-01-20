6f37e7a04e31bb6571d790ff783bef2c577ee96c
kxxoling <kxxoling@gmail.com>
2017-01-20 21:37:05 +0800

    Add list page for Every Page types.

--- home/models.py
+++ home/models.py
@@ -5,11 +5,42 @@ from posts.models import ArticlePage, GalleryPage, VideoPage, SharedLinkPage
 PAGE_TYPES = (ArticlePage, GalleryPage, VideoPage, SharedLinkPage,)
 
 
-class HomePage(Page):
-    subpage_types = list(PAGE_TYPES)
+class BaseIndexPage(Page):
+    _page_types = ()
+
+    class Meta:
+        abstract = True
 
     def get_context(self, request):
-        context = super(HomePage, self).get_context(request)
-        context['posts'] = Page.objects.type(PAGE_TYPES).live()
+        context = super(BaseIndexPage, self).get_context(request)
         context['recent_posts'] = Page.objects.type(PAGE_TYPES).live()
+        context['posts'] = Page.objects.type(self._page_types).live()
         return context
+
+    def get_template(self, request):
+        return 'home/home_page.html'
+
+
+def gen_list_page(model, app_label='', module='home'):
+    name = model.__name__.replace('Page', 'ListPage')
+
+    class Meta:
+        pass
+
+    if app_label:
+        # app_label must be set using the Meta inner class
+        setattr(Meta, 'app_label', app_label)
+
+    # Set up a dictionary to simulate declarations within a class
+    attrs = {'__module__': module, 'Meta': Meta, '_page_types': (model,)}
+
+    return type(name, (BaseIndexPage,), attrs)
+
+
+list_page_models = map(gen_list_page, PAGE_TYPES)      # (ArticleListPage, VideoListPage, GalleryListPage, SharedLinkListPage)
+
+
+class HomePage(BaseIndexPage):
+    _page_types = PAGE_TYPES
+    subpage_types = list_page_models
+
