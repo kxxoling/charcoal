107bd56256731d59ae086d3474a26079f45220ef
Kane Blueriver <kxxoling@gmail.com>
2017-05-04 01:18:06 +0800

    Add PROD mode for manage.py.

--- manage.py
+++ manage.py
@@ -4,8 +4,12 @@ from __future__ import absolute_import, unicode_literals
 import os
 import sys
 
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charcoal.settings.dev")
+    if os.environ.get('PROD'):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charcoal.settings.prod")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charcoal.settings.dev")
 
     from django.core.management import execute_from_command_line
 
