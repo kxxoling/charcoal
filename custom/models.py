9ca6a2cee022e116928e1b2bc7d89cc3f563b21f
Kane Blueriver <kxxoling@gmail.com>
2017-07-28 16:27:34 +0800

    Update custom image model fields.

--- custom/models.py
+++ custom/models.py
@@ -1,16 +1,29 @@
 # coding: utf-8
-from django.db.models import CharField, ForeignKey
+from django.db.models import CharField, ForeignKey, BooleanField, IntegerField
 from wagtail.wagtailimages.models import Image as _Image, Rendition as _Rendition
 from wagtail.wagtailimages.models import AbstractImage, AbstractRendition
 
 
 class SourcedImage(AbstractImage):
+    class Meta:
+        unique_together = ('pixiv_id', 'pixiv_order')
+
     orig_link = CharField(blank=True, null=True, max_length=300)
     source = CharField(blank=True, null=True, max_length=30)
 
+    pixiv_id = IntegerField(null=True, default=None, blank=True)
+    pixiv_order = IntegerField(null=True, default=0, blank=True)
+
+    is_restricted = BooleanField(null=False, default=False)
+
     admin_form_fields = _Image.admin_form_fields + (
-        'orig_link',
+        'orig_link', 'pixiv_id', 'pixiv_order', 'is_restricted',
     )
+
+    def get_pixiv_link(self):
+        if not self.pixiv_id:
+            return None
+        return 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=%d' % self.pixiv_id
 
 
 class ImageRendition(AbstractRendition):
