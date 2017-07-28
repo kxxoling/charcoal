a80e105bcc1422897b78f8768bd45dc34f41ba90
Kane Blueriver <kxxoling@gmail.com>
2017-07-28 18:23:11 +0800

    Return a 403 code for forbidden pages.

--- images/views.py
+++ images/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from wagtail.wagtailimages import get_image_model
@@ -12,6 +13,8 @@ Image = get_image_model()
 
 def show_images(request):
     image_list = Image.objects.order_by('-id')
+    if not request.user.is_superuser:
+        image_list = image_list.filter(is_restricted=False)
     paginator = Paginator(image_list, 24)
 
     page = request.GET.get('page', 1)
@@ -25,8 +28,11 @@ def show_images(request):
         'images': images,
     })
 
+
 def show_image(request, image_id):
     image = get_object_or_404(Image, pk=image_id)
+    if not request.user.is_superuser and image.is_restricted:
+        return HttpResponseForbidden()
     return render(request, 'images/image.html', {
         'img': image,
     })
