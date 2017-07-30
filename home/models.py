9dbfff9e227be954531bb7be0adf8828033baad2
Kane Blueriver <kxxoling@gmail.com>
2017-07-30 17:34:04 +0800

    Universal pagination template.

--- home/models.py
+++ home/models.py
@@ -1,4 +1,7 @@
+# coding: utf-8
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from wagtail.wagtailcore.models import Page
+
 from posts.models import ArticlePage, GalleryPage, VideoPage, SharedLinkPage
 
 
@@ -14,7 +17,18 @@ class BaseIndexPage(Page):
     def get_context(self, request):
         context = super(BaseIndexPage, self).get_context(request)
         context['recent_posts'] = Page.objects.type(PAGE_TYPES).live().order_by('-first_published_at')[:5]
-        context['posts'] = Page.objects.type(self._page_types).live().order_by('-first_published_at')[:10]
+
+        post_list = Page.objects.type(self._page_types).live().order_by('-first_published_at')
+        paginator = Paginator(post_list, 10)
+
+        page = request.GET.get('page', 1)
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+        context['posts'] = posts
         return context
 
     def get_template(self, request):
